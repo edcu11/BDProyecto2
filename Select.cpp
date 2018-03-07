b5e0b1688a5ec6aed41537a894ca16ece731eca3
Ubuntu <ubuntu@rails-dev-box>
2018-03-07 05:22:39 +0000

    problema con chars no se que pedos loco2

--- Select.cpp
+++ Select.cpp
@@ -4,10 +4,6 @@ using namespace std;
 
 bool SelectFromTable(std::vector<string> list)
 {
-  int tableIndexBlock = GetBitmapBlocksAmount(list[2]) + 2;
-  std::cout << "tableI: " << tableIndexBlock << '\n';
-
-  char * buffer = GetTableIndexData(list[2], tableIndexBlock);
 
 
 
